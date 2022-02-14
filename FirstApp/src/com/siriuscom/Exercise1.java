f9093a77733f600603df96f53c19f375d5b4d4cc
kuberavarman <amkuberavarman@gmail.com>
2022-02-14 14:56:54 +0530

    Create Exercise1.java
    Exercise1

---
+++ FirstApp/src/com/siriuscom/Exercise1.java
@@ -0,0 +1,13 @@
+package com.siriuscom;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=15;
+		int c=a*b;
+		System.out.println("The product is "+c);
+
+	}
+
+}
