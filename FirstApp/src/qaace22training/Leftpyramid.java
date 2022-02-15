4eced3e787bf32730591d12c29aacef09a7cca69
kuberavarman <amkuberavarman@gmail.com>
2022-02-15 17:54:15 +0530

    Day-2-Exercise

---
+++ FirstApp/src/qaace22training/Leftpyramid.java
@@ -0,0 +1,26 @@
+package qaace22training;
+
+public class Leftpyramid {
+
+	public static void main(String[] args)
+	{
+		int i, j, n = 5;
+		for (i=0; i<n; i++)
+		{
+		for (j=2*(n-i); j>=0; j--)
+		{
+		System.out.print(" ");
+		}
+		for (j=0; j<=i; j++)
+		{
+		System.out.print("* ");
+		}
+		System.out.println();
+		}
+		}
+
+     }
+	
+
+
+
---
+++ FirstApp/src/qaace22training/Pyramid.java
@@ -0,0 +1,25 @@
+package qaace22training;
+
+public class Pyramid {
+
+	public static void main(String[] args)
+	{
+		for (int i=0; i<=5 ;i++)
+		{
+		for (int j=5; j>i; j--)
+		{
+		System.out.print(" ");
+		}
+		for (int k=1; k<=i; k++)
+		{
+		System.out.print("* ");
+		}
+		System.out.println();
+		}
+
+
+
+		}
+	}
+
+
