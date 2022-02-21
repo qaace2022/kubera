42b36a6d86462256c2cdeed0d9735987f6998474
kuberavarman <amkuberavarman@gmail.com>
2022-02-21 17:52:29 +0530

    Day-6-Exercise

---
+++ FirstApp/src/qaace22training/Calculator.java
@@ -0,0 +1,48 @@
+package qaace22training;
+
+class Addition
+{
+	int c;
+	Addition(int a,int b)
+	{
+		c=a+b;
+	}
+}
+class Sub
+{
+	int c;
+	Sub(int a,int b)
+	{
+		c=a-b;
+	}
+}
+class Mult
+{
+	int c;
+	Mult(int a,int b)
+	{
+		c=a*b;
+	}
+}
+class Div
+{
+	int c;
+	Div(int a,int b)
+	{
+		c=a/b;
+	}
+}
+public class Calculator {
+	public static void main(String[] args) {
+
+		Addition add = new Addition(10,20);
+		System.out.println("Addition = "+add.c);
+		Sub sub = new Sub(10,5);
+		System.out.println("Subtraction ="+sub.c);
+		Mult mult = new Mult(10,20);
+		System.out.println("Multiplication ="+mult.c);
+		Div div = new Div(10,2);
+		System.out.println("Division ="+div.c);
+
+	}
+}
